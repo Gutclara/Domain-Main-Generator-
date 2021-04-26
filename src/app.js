576d765b133a3086c1ceaa246c14f1f942dea5f8
Gutclara <mariadelasclaras@gmail.com>
2021-04-26 14:51:56 +0000

    subo ejercicio pero le falta arreglar una cosita que se superpone

--- src/app.js
+++ src/app.js
@@ -6,6 +6,26 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let tld = [".us", ".com", ".net"];
+
+  document.getElementById("clic").addEventListener("click", function() {
+    let domains = "";
+    for (const p in pronoun) {
+      for (const a in adj) {
+        for (const n in noun) {
+          for (const d in tld) {
+            domains += pronoun[p] + adj[a] + noun[n] + tld[d] + "<br>";
+          }
+        }
+      }
+    }
+    let p = document.getElementById("domains");
+    p.innerHTML = domains;
+    console.log(domains);
+
+    p.hidden = false;
+  });
 };
